29158c7412c23ce5b691f1c8042423585fbf806b
Taiyanna <simstaiyanna@yahoo.com>
2021-07-21 16:59:05 -0400

    high order functions

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let plus= (num) => {
+    return (plusNum) => num + plusNum;
+}
+
+let plus15 = plus(15);
+console.log(plus15(10));
